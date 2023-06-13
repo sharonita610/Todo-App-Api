2260f52a988be72b74f521a568559d6570d52bc1
Sharon Kim <sharonita610@gmail.com>
2023-06-13 10:48:11 +0900

    토큰인증 로그인 처리

--- src/main/java/com/example/todo/auth/TokenProvider.java
+++ src/main/java/com/example/todo/auth/TokenProvider.java
@@ -1,6 +1,7 @@
 package com.example.todo.auth;
 
 import com.example.todo.userapi.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -55,7 +56,7 @@ public class TokenProvider {
         // 추가 클레임 정의
         Map<String, Object> claims = new HashMap<>();
         claims.put("email", userEntity.getEmail());
-        claims.put("role", userEntity.getRole());
+        claims.put("role", userEntity.getRole().toString());
 
         return Jwts.builder()
                 // 토큰 header에 들어갈 서명
@@ -64,12 +65,39 @@ public class TokenProvider {
                         , SignatureAlgorithm.HS512
                 )
                 // token payload에 들어갈 클레임 설정
+                .setClaims(claims) // 추가 클레임은 먼저 설정해야함
                 .setIssuer("바닐라 건듀") // iss : 발급자 정보
                 .setIssuedAt(new Date()) // iat : 발급 시간
                 .setExpiration(expiry) // exp : 토큰 만료 시간
                 .setSubject(userEntity.getId()) // sub : 토큰을 식별 할 수 있는 주요데이터
-                .setClaims(claims)
                 .compact();
+    }
+
+    /**
+     * 클라이언트가 전송한 토큰을 디코딩하여 토큰의 위조여부를 확인
+     * 토큰을 json 으로 파싱해서 클레임(토큰 정보)를 리턴
+     * @param token
+     * @return - 토큰 안에있는 인증된 유저 정보를 반환
+     */
+    public TokenUserInfo validateAndGetTokenUserInfo(String token){
+
+        Claims claims = Jwts.parserBuilder()
+                // 토큰 발급자의 발급 당시의 서명을 넣어줌
+                .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                // 서명 위조 검사 : 위조된 경우 예외가 발생함
+                // 위조가 되지 않았을 경우 클레임을 리턴
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        log.info("claims : {}", claims);
+
+        return TokenUserInfo.builder()
+                .userId(claims.getSubject()) // subject 안에다가 id를 넣었기 때문에 이렇게 사용한다
+                .email(claims.get("email", String.class))
+                .email(claims.get("role", String.class))
+                .build();
+
     }
 
 
--- src/main/java/com/example/todo/config/WebSecurityConfig.java
+++ src/main/java/com/example/todo/config/WebSecurityConfig.java
@@ -1,16 +1,23 @@
 package com.example.todo.config;
 
+import com.example.todo.filter.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 //@Configuration
 public class WebSecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     // 다른 사람이 만들어 둔 것을 주입 받을 때 필요하다. DI, dependency injection
     @Bean
@@ -19,6 +26,8 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
+    // 시큐리티 설정
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -27,9 +36,24 @@ public class WebSecurityConfig {
                 .and()
                 .csrf().disable()
                 .httpBasic().disable()
+                // 세션인증을 사용하지 않겠다
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //  세션 인증 안쓴다는 설정
+                .and()
+                // 어떤 요청에서 인증을 안할 것인지 설정, 언제 할 것인지 설정
+                .authorizeRequests().antMatchers("/", "/api/auth/**").permitAll()
+                .anyRequest().authenticated()
         ;
+
+        // 토큰인증 필터 연결
+        http.addFilterAfter(
+          jwtAuthFilter
+          , CorsFilter.class       // import 시 주의 : Spring 걸로 가져와야됨
+        );
 
         return http.build();
     }
+
+
 
 }
